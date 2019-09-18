844a282e11c28ed9a3e9287e78f2af828b2799ab
Erik MK <inboxtoerik@gmail.com>
2019-09-18 21:18:46 +0200

    added more elaborate examples of map and filter functions

--- filter_map_reduce.py
+++ filter_map_reduce.py
@@ -3,13 +3,15 @@
 
     filter() takes a function and list as arguments
     - filter returns a list of filtered items based on the lambda function
+    - wrap the filter object in a list function
         ex:
-        filter(lambda function, list)
+        list(filter(lambda function, list))
 
     map() takes a function and list as arguments
     - map returns a list of modified items based on the lambda function
+    - wrap the map object in a list function
         ex:
-        map(lambda function, list)
+        list(map(lambda function, list))
 
     reduce() takes a function and list as arguments
     - reduce performs a repetitive operation over all the elements in the list
@@ -35,3 +37,70 @@ print('mapped list - doubled numbers: ', final_list)
 # reduce example - sum all numbers
 result = reduce((lambda x, y: x + y), li)
 print('reduced list - sum of all: ', result)
+
+print('')
+
+# more lambda examples
+
+# list of dictionaries with programming languages and frameworks
+# framework is empty for language main name
+programming_lanuages = [
+    {'language': 'python', 'framework': 'flask'}, 
+    {'language': 'python', 'framework': 'django'}, 
+    {'language': 'python', 'framework': ''}, 
+    {'language': 'php', 'framework': 'laravel'},
+    {'language': 'php', 'framework': 'symfony'},
+    {'language': 'php', 'framework': ''},
+    {'language': 'php', 'framework': 'codeigniter'},
+    {'language': 'javascript', 'framework': 'react'},
+    {'language': 'javascript', 'framework': 'angular'},
+    {'language': 'javascript', 'framework': ''},
+    {'language': 'javascript', 'framework': 'vue'},
+    {'language': 'javascript', 'framework': 'nodejs'},
+    {'language': 'java', 'framework': 'blade'},
+    {'language': 'java', 'framework': ''},
+    {'language': 'java', 'framework': 'spark'},
+    {'language': 'java', 'framework': 'spring'},
+    {'language': 'ruby', 'framework': 'rails'},
+    {'language': 'swift', 'framework': ''},
+    {'language': 'go', 'framework': ''},
+    {'language': 'c#', 'framework': ''},
+    {'language': 'c#', 'framework': 'asp.net'},
+]
+
+##############################################################################
+# FIND ALL LANGUAGE NAMES AND PRINT THEM
+##############################################################################
+# first operation returns a filtered list of entries where framework is empty
+languages = list(filter(lambda x: x['framework'] =='', programming_lanuages))
+# output: [{'language': 'python', 'framework': ''}, {'language': 'php', 'framework': ''}, {'language': 'javascript', 'framework': ''}, {'language': 'java', 'framework': ''}, {'language': 'swift', 'framework': ''}, {'language': 'go', 'framework': ''}, {'language': 'c#', 'framework': ''}]
+# second operations returns a list with the language name extraxted from
+# the dictionary and joins the list to a comma separeted string
+print('List of all programming languages:')
+print(', '.join(list(map(lambda x: x['language'], languages)))) 
+# output: python, php, javascript, java, swift, go, c#
+
+##############################################################################
+# FIND ALL FRAMEWORK NAMES AND PRINT THEM IN UPPER CASE
+##############################################################################
+# first operation returns a list of frameworks in uppercase
+frameworks_upper = list(map(lambda x : x['framework'].upper(),  programming_lanuages))
+# output: ['FLASK', 'DJANGO', '', 'LARAVEL', 'SYMFONY', '', 'CODEIGNITER', 'REACT', 'ANGULAR', '', 'VUE', 'NODEJS', 'BLADE', '', 'SPARK', 'SPRING', 'RAILS', '', '', '', 'ASP.NET']
+# second operation returns a filtered list with empty entries omitted
+# and prints it as a joined comma separated string
+print('\nList of all frameworks in UPPER CASE:')
+print(', '.join(list(filter(lambda x: x != '', frameworks_upper)))) 
+# Output: FLASK, DJANGO, LARAVEL, SYMFONY, CODEIGNITER, REACT, ANGULAR, VUE, NODEJS, BLADE, SPARK, SPRING, RAILS, ASP.NET
+
+##############################################################################
+# FIND ALL FRAMEWORKS RELATED TO JAVASCRIPT
+##############################################################################
+# first operation returns a list of entries where language is javascript
+# and framework is not empty
+js_frameworks = list(filter(lambda x: x['language'] == 'javascript' and x['framework'] != '', programming_lanuages))
+# output: [{'language': 'javascript', 'framework': 'react'}, {'language': 'javascript', 'framework': 'angular'}, {'language': 'javascript', 'framework': 'vue'}, {'language': 'javascript', 'framework': 'nodejs'}]
+# second operation extracts the framework names in a list and joins it to
+# a comma separated string
+print('\nList of Javascript related frameworks:')
+print(', '.join(list(map(lambda x: x['framework'], js_frameworks))))
+# output: react, angular, vue, nodejs
